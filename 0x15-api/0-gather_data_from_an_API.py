5173530c33ef7615a8a86af82cb5edb6ffa4b27c
azarelmasry <azarelmasry@gmail.com>
2024-02-19 05:31:24 -0800

    0.api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,37 +1,14 @@
 #!/usr/bin/python3
-"""
-This script uses a REST API to retrieve information about
-a given employee's TODO list progress.
-"""
-from requests import get
-from sys import argv
+"""Returns to-do list information for a given employee ID."""
+import requests
+import sys
 
-if __name__ == '__main__':
-    # Get the user ID as a command-line argument
-    userId = argv[1]
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    # Retrieve user information from the API
-    user = get("https://jsonplaceholder.typicode.com/users/{}".format(userId)) 
-    # Extract the user's name from the response
-    name = user.json().get('name')
-
-    # Retrieve TODO list data from the API
-    todos = get('https://jsonplaceholder.typicode.com/todos')
-    
-    # Initialize variables to keep track of completed and total tasks
-    totalTasks = 0
-    completed = 0
-
-    # Iterate through the TODO list to count completed tasks
-    for task in todos.json():
-        if task.get('userId') == int(userId):
-            totalTasks += 1
-            if task.get('completed'):
-                completed += 1
-
-    # Print the progress information
-    print('Employee {} has completed tasks ({}/{}) :'.format(name, completed, totalTasks))
-
-    # Print the titles of completed tasks
-    print('\n'.join(["\t " + task.get('title') for task in todos.json()
-                     if task.get('userId') == int(userId) and task.get('completed')]))
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
