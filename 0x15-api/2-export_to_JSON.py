e3c97a55651229501966a6415ed5a8d09557a691
azarelmasry <azarelmasry@gmail.com>
2024-02-19 05:37:06 -0800

    0.api

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,40 +1,19 @@
 #!/usr/bin/python3
-"""Exports data in JSON format."""
+"""Exports to-do list information for a given employee ID to JSON format."""
+import json
+import requests
+import sys
 
 if __name__ == "__main__":
-    import json
-    import requests
-    import sys
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    # Get the user ID from command-line arguments
-    userId = sys.argv[1]
-
-    # Fetch user data from the API
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(userId))
-
-    # Fetch TODO list data from the API and parse it as JSON
-    todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
-
-    # Initialize dictionaries and lists for organizing the data
-    todoUser = {}
-    taskList = []
-
-    # Iterate through TODO list items and extract relevant information
-    for task in todos:
-        if task.get('userId') == int(userId):
-            taskDict = {
-                "task": task.get('title'),
-                "completed": task.get('completed'),
-                "username": user.json().get('username')
-            }
-            taskList.append(taskDict)
-    
-    # Organize the data under the user's ID
-    todoUser[userId] = taskList
-
-    # Define the filename for the JSON export
-    filename = userId + '.json'
-    
-    # Write the JSON data to the specified file
-    with open(filename, mode='w') as f:
-        json.dump(todoUser, f)
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
+            } for t in todos]}, jsonfile)
